95a22bb9fa9bfd346ec57746b22264f1551c89b6
Photon Cat <lkstc112233@gmail.com>
2018-07-15 14:31:27 -0700

    Package rename.

---
+++ src/com/photoncat/twodots/Controller.java
@@ -0,0 +1,4 @@
+package com.photoncat.twodots;
+
+public class Controller {
+}
--- src/com/photoncat/twodots/Main.java
+++ src/com/photoncat/twodots/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.photoncat.twodots;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
